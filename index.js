309a2b8c569988e2e137fd69ac83fbd441e775b3
Charles Uka <121673088+Charletontech@users.noreply.github.com>
2025-11-28 17:13:51 +0100

    Comment out database connection and cron job
    Comment out database connection and cron job loading.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const loanRoutes = require("./routes/loan.routes");
 const adminRoutes = require("./routes/admin.routes");
 const notificationRoutes = require("./routes/notification.routes");
 const investmentRoutes = require("./routes/investment.routes.js");
-require("./cron-jobs.js");
+//require("./cron-jobs.js");
 const Database = require("./loaders/database");
 
 const app = express();
@@ -40,10 +40,10 @@ app.get("/", (req, res) => {
 });
 
 // connect database
-Database.connect();
+//Database.connect();
 
 // synchronize database
-Database.sync();
+//Database.sync();
 
 // start running server
 app.listen(port, () => {
